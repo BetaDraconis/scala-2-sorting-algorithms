7b5ef8fd62cbfd5aa0c24c7fd032b7d43eedb4db
BetaDraconis <47660116+BetaDraconis@users.noreply.github.com>
2022-04-12 21:50:41 +0100

    merge-sort: Add working implementation of MergeSort.

--- app/v1/sorts/MergeSort.scala
+++ app/v1/sorts/MergeSort.scala
@@ -16,6 +16,51 @@
 
 package v1.sorts
 
+import scala.annotation.tailrec
+
 object MergeSort extends Sort {
-  override protected[sorts] def sort(nums: Seq[BigDecimal]): Seq[BigDecimal] = nums
+  override protected[sorts] def sort(nums: Seq[BigDecimal]): Seq[BigDecimal] = doSort(nums)
+
+  def doSort(nums: Seq[BigDecimal]): Seq[BigDecimal] = {
+    @tailrec
+    def split(nums: Seq[BigDecimal],
+              leftNums: Option[Seq[BigDecimal]] = None,
+              rightNums: Option[Seq[BigDecimal]] = None,
+              middleNums: Option[BigDecimal] = None): Seq[Seq[BigDecimal]] =
+      nums match {
+      case Nil => Seq(leftNums, rightNums).flatten
+      case num :: Nil => Seq(leftNums, Some(Seq(num)), rightNums).flatten
+      case _ =>
+        val newNums: Seq[BigDecimal] = nums.drop(1).dropRight(1)
+        val nextLeftNum: Seq[BigDecimal] = nums.take(1)
+        val nextRightNum: Seq[BigDecimal] = nums.takeRight(1)
+        val newLeftNums: Seq[BigDecimal] = leftNums.fold(nextLeftNum)(nums => nums ++ nextLeftNum)
+        val newRightNums: Seq[BigDecimal] = rightNums.fold(nextRightNum)(nums => nextRightNum ++ nums)
+        split(newNums, Some(newLeftNums), Some(newRightNums), None)
+    }
+
+    @tailrec
+    def combine(leftNums: Seq[BigDecimal], rightNums: Seq[BigDecimal], sortedNums: Seq[BigDecimal] = Nil): Seq[BigDecimal] = {
+      (leftNums, rightNums) match {
+        case (Nil, Nil) => sortedNums
+        case (_, Nil) => sortedNums ++ leftNums
+        case (Nil, _) => sortedNums ++ rightNums
+        case (_, _) =>
+          val rightHead: BigDecimal = rightNums.head
+          val insertionPoint = leftNums.indexWhere(item => rightHead <= item)
+
+          if (insertionPoint == -1) sortedNums ++ leftNums ++ rightNums
+          else {
+            val newSortedNums: Seq[BigDecimal] = sortedNums ++ leftNums.take(insertionPoint) :+ rightHead
+            combine(leftNums.drop(insertionPoint), rightNums.tail, newSortedNums)
+          }
+      }
+    }
+
+    nums match {
+      case Nil => Nil
+      case num :: Nil => nums
+      case _ => split(nums).map(doSort).fold(Seq.empty[BigDecimal])((leftNums, rightNums) => combine(leftNums, rightNums))
+    }
+  }
 }
