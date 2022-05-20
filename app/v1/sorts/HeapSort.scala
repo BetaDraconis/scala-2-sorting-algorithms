a3784a5dad2e1c82729637c23156b7fb6dd97cf9
lukejones <47660116+BetaDraconis@users.noreply.github.com>
2022-05-20 17:10:04 +0100

    heap-sort: long-winded implementation WIP

--- app/v1/sorts/HeapSort.scala
+++ app/v1/sorts/HeapSort.scala
@@ -26,24 +26,49 @@ object HeapSort extends Sort {
       case num :: Nil => num +: sortedNums
       case _ =>
         val heapedUnsortedNums: Seq[BigDecimal] = heapify(unsortedNums)
-        doSort(heapedUnsortedNums.head +: sortedNums, reHeapify(heapedUnsortedNums.tail))
+        doSort(reHeapify(heapedUnsortedNums.tail), heapedUnsortedNums.head +: sortedNums)
     }
 
     def heapify(nums: Seq[BigDecimal], index: Int = 0): Seq[BigDecimal] = {
       val parentNode: BigDecimal = nums(index)
-      val leftChildNodeExists: Boolean = nums.isDefinedAt((2*index) + 1)
-      val rightChildNodeExists: Boolean = nums.isDefinedAt((2*index) + 2)
+
+      val leftChildIndex: Int = (2 * index) + 1
+      val rightChildIndex: Int = (2 * index) + 2
+      val leftChildNodeExists: Boolean = nums.isDefinedAt(leftChildIndex)
+      val rightChildNodeExists: Boolean = nums.isDefinedAt(rightChildIndex)
+
+      def sortParentWithChildAtIndex(childIndex: Int): Seq[BigDecimal] =
+        if (nums(childIndex) > parentNode) swap(nums, index, childIndex) else nums
 
       (leftChildNodeExists, rightChildNodeExists) match {
-        case (false, false) => nums
-        case (true, true) => nums
+        case (false, false) => nums // the bottom of the heap
+        case (true, false) => sortParentWithChildAtIndex(leftChildIndex)
+        case (false, true) => sortParentWithChildAtIndex(rightChildIndex)
+        case (true, true) =>
+          val a = heapify(nums, leftChildIndex)
+          val b = heapify(nums, rightChildIndex)
+          val c = nums.zipWithIndex
 
+          val difs = c.diff(a) ++ c.diff(b) // could be improved by checking only diffs in child heap indexes
+
+          val updatedNums: Seq[BigDecimal] = c.map{ (aa) =>
+            val as = difs.indexWhere(_._2 == aa._2)
+            if (as == -1) aa._1 else difs(as)._1
+          }
+
+          val newLeftChild = updatedNums(leftChildIndex)
+          val newRightChild = updatedNums(rightChildIndex)
+
+          if (newLeftChild > parentNode && newLeftChild > newRightChild) swap(updatedNums, index, leftChildIndex)
+          else if (newRightChild > parentNode && newRightChild > newLeftChild) swap(updatedNums, index, rightChildIndex)
+          else updatedNums
       }
-      nums
     }
 
-
-    def reHeapify(nums: Seq[BigDecimal]): Seq[BigDecimal] = nums
+    def reHeapify(nums: Seq[BigDecimal]): Seq[BigDecimal] = {
+      heapify(nums.takeRight(1) ++ nums.dropRight(1))
+      // you only actually have to re-heap the side of the heap with the highest top-level value (index = 1 or 2)
+    }
 
     doSort(list)
   }
