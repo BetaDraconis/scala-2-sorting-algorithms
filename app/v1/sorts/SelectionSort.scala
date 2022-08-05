c257d03ea039e2c72aae4638c68c91b12eea264f
luke.jones <47660116+BetaDraconis@users.noreply.github.com>
2022-08-06 00:29:19 +0100

    SSA-010: Implement selection sort (fold).

--- app/v1/sorts/SelectionSort.scala
+++ app/v1/sorts/SelectionSort.scala
@@ -8,30 +8,20 @@ object SelectionSort extends Sort {
   @tailrec
   def doSort(unsortedNums: Seq[BigDecimal], sortedNums: Seq[BigDecimal] = Nil): Seq[BigDecimal] = {
 
-    // TODO: Replace this with a fold
-    @tailrec
-    def findLowest(currLowestNums: Seq[BigDecimal], uncheckedNums: Seq[BigDecimal], checkedNums: Seq[BigDecimal] = Nil): (Seq[BigDecimal], Seq[BigDecimal]) = {
-      val currLowestNum: BigDecimal = currLowestNums.head
-
-      def checkNum(num: BigDecimal): (Seq[BigDecimal], Seq[BigDecimal]) =
-        if (num > currLowestNum) (currLowestNums, checkedNums :+ num)
-        else if (num == currLowestNum) (currLowestNums :+ num, checkedNums)
-        else (Seq(num), currLowestNums ++ checkedNums)
-
-      uncheckedNums match {
-        case Nil => (currLowestNums, checkedNums)
-        case head :: Nil => checkNum(head)
-        case head :: tail =>
-          val (newLowestNums, newCheckedNums) = checkNum(head)
-          findLowest(newLowestNums, tail, newCheckedNums)
-      }
-    }
-
     unsortedNums match {
       case Nil => sortedNums
       case num :: Nil => sortedNums :+ num
       case head :: tail =>
-        val (lowestNums, remainingNums) = findLowest(Seq(head), tail)
+        val (lowestNums, remainingNums) = tail.foldLeft((Seq(head), Seq.empty[BigDecimal]))((checkState, nextNum) => {
+
+          val (currLowestNums, checkedNums) = checkState
+          val currLowestNum: BigDecimal = currLowestNums.head
+
+          if (nextNum > currLowestNum) (currLowestNums, checkedNums :+ nextNum)
+          else if (nextNum == currLowestNum) (currLowestNums :+ nextNum, checkedNums)
+          else (Seq(nextNum), currLowestNums ++ checkedNums)
+        })
+
         doSort(remainingNums, sortedNums ++ lowestNums)
     }
   }
