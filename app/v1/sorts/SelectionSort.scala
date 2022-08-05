e97247d0b33ea5d06158d996a9588a057f1cb400
luke.jones <47660116+BetaDraconis@users.noreply.github.com>
2022-08-05 23:10:11 +0100

    SSA-010: Add placeholder classes for SelectionSort.

---
+++ app/v1/sorts/SelectionSort.scala
@@ -0,0 +1,5 @@
+package v1.sorts
+
+object SelectionSort extends Sort {
+  override protected[sorts] def sort(list: Seq[BigDecimal]): Seq[BigDecimal] = list
+}
---
+++ test/v1/sorts/SelectionSortSpec.scala
@@ -0,0 +1,7 @@
+package v1.sorts
+
+import support.SortingSpec
+
+class SelectionSortSpec extends SortingSpec {
+  sortingSmokeTest(SelectionSort.sort)
+}
