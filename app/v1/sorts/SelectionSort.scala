7040602ecfcc4020b178ca626b511403a0505e2e
luke.jones <47660116+BetaDraconis@users.noreply.github.com>
2022-08-06 00:36:50 +0100

    SSA-010: Minor reformatting + re-adding functional method for finding next lowest values to compare performance.

--- app/v1/sorts/SelectionSort.scala
+++ app/v1/sorts/SelectionSort.scala
@@ -3,12 +3,29 @@ package v1.sorts
 import scala.annotation.tailrec
 
 object SelectionSort extends Sort {
-  override protected[sorts] def sort(list: Seq[BigDecimal]): Seq[BigDecimal] = doSort(list)
+  override protected[sorts] def sort(list: Seq[BigDecimal]): Seq[BigDecimal] = {
 
-  @tailrec
-  def doSort(unsortedNums: Seq[BigDecimal], sortedNums: Seq[BigDecimal] = Nil): Seq[BigDecimal] = {
+    //TODO: Compare performance of this method vs fold implementation
+    @tailrec
+    def findLowest(currLowestNums: Seq[BigDecimal], uncheckedNums: Seq[BigDecimal], checkedNums: Seq[BigDecimal] = Nil): (Seq[BigDecimal], Seq[BigDecimal]) = {
+      val currLowestNum: BigDecimal = currLowestNums.head
 
-    unsortedNums match {
+      def checkNum(num: BigDecimal): (Seq[BigDecimal], Seq[BigDecimal]) =
+        if (num > currLowestNum) (currLowestNums, checkedNums :+ num)
+        else if (num == currLowestNum) (currLowestNums :+ num, checkedNums)
+        else (Seq(num), currLowestNums ++ checkedNums)
+
+      uncheckedNums match {
+        case Nil => (currLowestNums, checkedNums)
+        case head :: Nil => checkNum(head)
+        case head :: tail =>
+          val (newLowestNums, newCheckedNums) = checkNum(head)
+          findLowest(newLowestNums, tail, newCheckedNums)
+      }
+    }
+
+    @tailrec
+    def doSort(unsortedNums: Seq[BigDecimal], sortedNums: Seq[BigDecimal] = Nil): Seq[BigDecimal] = unsortedNums match {
       case Nil => sortedNums
       case num :: Nil => sortedNums :+ num
       case head :: tail =>
@@ -24,5 +41,7 @@ object SelectionSort extends Sort {
 
         doSort(remainingNums, sortedNums ++ lowestNums)
     }
+
+    doSort(list)
   }
 }
