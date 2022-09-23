55bb271404e79880a61221ac1792b77af846ab53
luke.jones <47660116+BetaDraconis@users.noreply.github.com>
2022-09-23 16:33:04 +0100

    WIP

--- app/v1/sorts/ShellSort.scala
+++ app/v1/sorts/ShellSort.scala
@@ -12,47 +12,50 @@ object ShellSort extends Sort {
     }
 
     @tailrec
-    def sortWithGap(nums: Seq[BigDecimal], gap: Int, startingIndex: Int = 0): Seq[BigDecimal] = {
-      val nextIndex = startingIndex + gap
+    def doSort(nums: Seq[BigDecimal], gaps: Seq[Int]): Seq[BigDecimal] = {
 
       @tailrec
-      def sortInSubList(nums: Seq[BigDecimal],
-                        lastValidIndex: Int = nextIndex,
-                        subListIndexes: Seq[Int] = Seq(startingIndex, nextIndex)): Seq[BigDecimal] = {
-
-        val nextSubListIndex = lastValidIndex + gap
+      def sortWithGap(nums: Seq[BigDecimal], gap: Int, startingIndex: Int = 0): Seq[BigDecimal] = {
+        val nextIndex = startingIndex + gap
 
         @tailrec
-        def placeNumInSubList(numToPlace: BigDecimal,
-                              indexes: Seq[Int] = subListIndexes,
-                              nums: Seq[BigDecimal] = nums): Seq[BigDecimal] = indexes match {
-          case Nil => nums
-          case _ :+ last if numToPlace >= nums(last) => nums
-          case allButLast :+ last => placeNumInSubList(numToPlace, allButLast, swap(nums, last, nextSubListIndex))
+        def sortInSubList(nums: Seq[BigDecimal],
+                          lastValidIndex: Int = nextIndex,
+                          subListIndexes: Seq[Int] = Seq(startingIndex, nextIndex)): Seq[BigDecimal] = {
+
+          val nextSubListIndex = lastValidIndex + gap
+
+          @tailrec
+          def placeNumInSubList(numToPlace: BigDecimal,
+                                indexes: Seq[Int] = subListIndexes,
+                                nums: Seq[BigDecimal] = nums): Seq[BigDecimal] = indexes match {
+            case Nil => nums
+            case _ :+ last if numToPlace >= nums(last) => nums
+            case allButLast :+ last => placeNumInSubList(numToPlace, allButLast, swap(nums, last, nextSubListIndex))
+          }
+
+          nums.drop(nextSubListIndex) match {
+            case Nil => nums
+            case numToPlace :: Nil => placeNumInSubList(numToPlace)
+            case numToPlace :: _ => sortInSubList(placeNumInSubList(numToPlace), nextSubListIndex, subListIndexes :+ nextSubListIndex)
+          }
         }
 
-        nums.drop(nextSubListIndex) match {
-          case Nil => nums
-          case numToPlace :: Nil => placeNumInSubList(numToPlace)
-          case numToPlace :: _ => sortInSubList(placeNumInSubList(numToPlace), nextSubListIndex, subListIndexes :+ nextSubListIndex)
+        val maxRequiredIndex = gap - 1
+
+        (nums.drop(nextIndex), startingIndex) match {
+          case (Nil, _) => nums
+          case (_ :: Nil, _) => sortInSubList(nums)
+          case (_, `maxRequiredIndex`) => sortInSubList(nums)
+          case _ => sortWithGap(sortInSubList(nums), gap, startingIndex + 1)
         }
       }
 
-      val ab = gap - 1
-
-      (nums.drop(nextIndex), startingIndex) match {
-        case (Nil, _) => nums
-        case (_ :: Nil, _) => sortInSubList(nums)
-        case (_, `ab`) => sortInSubList(nums)
-        case _ => sortWithGap(sortInSubList(nums), startingIndex + 1)
+      gaps match {
+        case Nil => nums
+        case gap :: Nil => sortWithGap(nums, gap)
+        case gap :: tail => doSort(sortWithGap(nums, gap), tail)
       }
-    }
-
-    @tailrec
-    def doSort(nums: Seq[BigDecimal], gaps: Seq[Int]): Seq[BigDecimal] = gaps match {
-      case Nil => nums
-      case gap :: Nil => sortWithGap(nums, gap)
-      case gap :: tail => doSort(sortWithGap(nums, gap), tail)
     }
 
     doSort(list, generateGaps())
