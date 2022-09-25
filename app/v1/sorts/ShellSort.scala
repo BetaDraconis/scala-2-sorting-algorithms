046cfa91288e3abcafc557ef080c015cf0d12679
Luke.Jones Work Account <47660116+BetaDraconis@users.noreply.github.com>
2022-09-25 22:50:14 +0100

    SSA-011: Starting rewriting ShellSort mechanism as per comment.

--- app/v1/sorts/ShellSort.scala
+++ app/v1/sorts/ShellSort.scala
@@ -14,40 +14,30 @@ object ShellSort extends Sort {
     @tailrec
     def doSort(nums: Seq[BigDecimal], gaps: Seq[Int]): Seq[BigDecimal] = {
 
-      @tailrec
-      def sortWithGap(nums: Seq[BigDecimal], gap: Int, startingIndex: Int = 0): Seq[BigDecimal] = {
-        val nextIndex = startingIndex + gap
+      //@tailrec
+      def sortWithGap(nums: Seq[BigDecimal], gap: Int, startingIndex: Int = 0, currIndex: Int = 0, prevIndexes: Seq[Int] = Nil): Seq[BigDecimal] = {
+        /*
+          I want to do this in one step (rather than multiple nested recursive functions) following the logic:
+            - for each new item in a sublist you can immediately place it rather than storing the index and then
+              sorting it later.
+            - once you hit the end of a sublist you then move onto the next subList at startingIndex += 1 until either
+              no more sublists are possible, or you have created #gap sublists
+            - you never need more than #gap sublists. This is because each sublist will contain 1/gap total possible
+              items, i.e for a gap of two the first sublist will contain all even numbers, and the second all odd numbers.
+         */
 
-        @tailrec
-        def sortInSubList(nums: Seq[BigDecimal],
-                          lastValidIndex: Int = nextIndex,
-                          subListIndexes: Seq[Int] = Seq(startingIndex, nextIndex)): Seq[BigDecimal] = {
+        val isSortedWithGap: Boolean =
+          startingIndex == gap ||
+          (startingIndex == currIndex && !nums.isDefinedAt(startingIndex + gap))
 
-          val nextSubListIndex = lastValidIndex + gap
-
-          @tailrec
-          def placeNumInSubList(numToPlace: BigDecimal,
-                                indexes: Seq[Int] = subListIndexes,
-                                nums: Seq[BigDecimal] = nums): Seq[BigDecimal] = indexes match {
-            case Nil => nums
-            case _ :+ last if numToPlace >= nums(last) => nums
-            case allButLast :+ last => placeNumInSubList(numToPlace, allButLast, swap(nums, last, nextSubListIndex))
-          }
-
-          nums.drop(nextSubListIndex) match {
-            case Nil => nums
-            case numToPlace :: Nil => placeNumInSubList(numToPlace)
-            case numToPlace :: _ => sortInSubList(placeNumInSubList(numToPlace), nextSubListIndex, subListIndexes :+ nextSubListIndex)
-          }
-        }
-
-        val maxRequiredIndex = gap - 1
-
-        (nums.drop(nextIndex), startingIndex) match {
-          case (Nil, _) => nums
-          case (_ :: Nil, _) => sortInSubList(nums)
-          case (_, `maxRequiredIndex`) => sortInSubList(nums)
-          case _ => sortWithGap(sortInSubList(nums), gap, startingIndex + 1)
+        if (isSortedWithGap) {
+          nums
+        } else {
+          /*
+            for each starting index increase currIndex by gap until you hit the end of the subList and then increment
+            starting index by 1 until the above Boolean condition indicates that the sort is complete for a gap
+           */
+          nums
         }
       }
 
