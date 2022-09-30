b23316aebd9c4fdeb558dde246d72616fd277827
Luke Jones <47660116+BetaDraconis@users.noreply.github.com>
2022-09-30 18:07:34 +0100

    SSA 011 (#14)
    * SSA-011: Initial (not-working) implementation of ShellSort. See TODO for information on what needs to be changed.
    
    * SSA-011: Initial (not-working) implementation of ShellSort. See TODO for information on what needs to be changed.
    
    * SSA-011: Working (but very inefficient) implementation of ShellSort.
    
    * SSA-011: rewrite ShellSort WIP.
    
    * WIP
    
    * SSA-011: Starting rewriting ShellSort mechanism as per comment.
    
    * SSA-011: Functional (if unsightly) ShellSort implementation. Still needs to be cleaned up, and made to work with fewer loops of sortWithGap
    
    * SSA-011: Replace nested if statements with pattern matching and simplify some logic
    
    * SSA-011: Replace fold with method to place items into the correct index.
    
    * SSA-011: Rebase fixes.

---
+++ app/v1/sorts/ShellSort.scala
@@ -0,0 +1,50 @@
+package v1.sorts
+
+import scala.annotation.tailrec
+
+object ShellSort extends Sort[BigDecimal] {
+  override protected[sorts] def sort(list: Seq[BigDecimal]): Seq[BigDecimal] = {
+
+    @tailrec
+    def generateGaps(gaps: Seq[Int] = Nil, i: Int = 1): Seq[Int] = {
+      val nextGap = (2 * i) - 1
+      if (list.drop(nextGap).nonEmpty) generateGaps(nextGap +: gaps, i + 1) else gaps
+    }
+
+    @tailrec
+    def doSort(nums: Seq[BigDecimal], gaps: Seq[Int]): Seq[BigDecimal] = {
+
+      @tailrec
+      def placeItem(nums: Seq[BigDecimal], indexToPlace: Int, indexesToSwapWith: Seq[Int]): Seq[BigDecimal] = indexesToSwapWith match {
+        case Nil => nums
+        case Nil :+ last => swap(nums, last, indexToPlace)
+        case rest :+ last => placeItem(swap(nums, last, indexToPlace), last, rest)
+      }
+
+      @tailrec
+      def sortWithGap(nums: Seq[BigDecimal], gap: Int, sublistIndex: Int = 0, currIndex: Int = 0, prevIndexes: Seq[Int] = Nil): Seq[BigDecimal] = {
+        val nextSublistIndex: Int = currIndex + gap
+
+        val isSortedWithGap: Boolean = nums.drop(sublistIndex + gap).isEmpty || sublistIndex == gap
+        val isEndOfSublist: Boolean = nums.drop(nextSublistIndex).isEmpty
+
+        (isSortedWithGap, isEndOfSublist) match {
+          case (true, _) => nums
+          case (_, true) => sortWithGap(nums, gap, sublistIndex + 1, sublistIndex + 1)
+          case (_, _) =>
+            val indexesToSwap = (prevIndexes :+ currIndex).dropWhile(i => nums(i) <= nums(nextSublistIndex))
+            val newNums = placeItem(nums, nextSublistIndex, indexesToSwap)
+            sortWithGap(newNums, gap, sublistIndex, nextSublistIndex, prevIndexes :+ currIndex)
+        }
+      }
+
+      gaps match {
+        case Nil => nums
+        case gap :: Nil => sortWithGap(nums, gap)
+        case gap :: tail => doSort(sortWithGap(nums, gap), tail)
+      }
+    }
+
+    doSort(list, generateGaps())
+  }
+}
