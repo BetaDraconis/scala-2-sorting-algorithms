8c70686a43eb2420c0e01115b4af24cef8ba1f93
Luke Jones <47660116+BetaDraconis@users.noreply.github.com>
2022-10-02 17:19:00 +0100

    SSA-017: Fix ExchangeSort. (#15)

--- app/v1/sorts/ExchangeSort.scala
+++ app/v1/sorts/ExchangeSort.scala
@@ -9,16 +9,16 @@ object ExchangeSort extends Sort[BigDecimal] {
     def doSort(nums: Seq[BigDecimal], index: Int = 0): Seq[BigDecimal] = {
 
       @tailrec
-      def sortValueAtIndex(nums: Seq[BigDecimal], index: Int = 1): Seq[BigDecimal] = nums.drop(index) match {
-          case Nil => nums
-          case item :: Nil => if (nums.head > item) swap(nums, index1 = 0, index2 = index) else nums
-          case item :: _ => if (nums.head > item) sortValueAtIndex(swap(nums, index1 = 0, index2 = index), index + 1) else sortValueAtIndex(nums, index + 1)
-        }
+      def doNextExchange(nums: Seq[BigDecimal], index: Int = 1): Seq[BigDecimal] = nums.drop(index) match {
+        case Nil => nums
+        case head :: _ if nums.head > head => doNextExchange(swap(nums, 0, index), index + 1)
+        case _ => doNextExchange(nums, index + 1)
+      }
 
       nums.drop(index) match {
         case Nil => nums
         case _ :: Nil => nums
-        case numsToSort => doSort(numsToSort.take(index) ++ sortValueAtIndex(numsToSort), index + 1)
+        case numsToSort => doSort(nums.take(index) ++ doNextExchange(numsToSort), index + 1)
       }
     }
 
